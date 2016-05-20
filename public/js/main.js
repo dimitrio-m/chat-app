edc14e1789717843826c5f3705b44f868c74d74a
Dimitrio Mandamadiotis <dimitriosmandamadiotis@gmail.com>
2016-05-20 16:44:21 -0400

    Agregado bower y cambio de diseño, agregado nicknames.

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,17 @@
 var socket = io();
 
+window.sessionStorage.nickname = prompt("Elige tu nombre de Usuario: ");
+$('#usuario').text(window.sessionStorage.nickname);
+window.sessionStorage.color = prompt("Elige el color de tu nombre:  \nSoportamos: azul, verde, rojo, morado, rosado, anaranjado, marron.");
+
 $("form").submit(function () {
-  socket.emit("chat message", $('#m').val());
+  socket.emit("chat message", "<span class=" + window.sessionStorage.color + ">" + window.sessionStorage.nickname + "</span>" + ": " + $('#m').val());
   $('#m').val("");
   return false;
 });
 
 socket.on('chat message', function(msg){
-   $('#messages').append($('<li>').text(msg));
+   $('#messages').append($('<li>').html( msg));
+   var elem = document.getElementById('content');
+   elem.scrollTop = elem.scrollHeight;
  });
